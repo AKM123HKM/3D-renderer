53ee14cd9d3f43c19895dbad2ba92a440d9e8594
AKM123HKM <ankitcoder1987@gmail.com>
2026-09-18 05:20:10 +0530

    implemented back face culling

--- src/rasterizer/main.cpp
+++ src/rasterizer/main.cpp
@@ -8,6 +8,7 @@ constexpr int WIDTH = 800;
 constexpr int HEIGHT = 800;
 constexpr float VIEWPORT_W = 2;
 constexpr float VIEWPORT_H = 2;
+using DEPTH_BUFFER = std::vector<std::vector<float>>;
 constexpr float pi = 3.14159;
 using Matrix = std::array<std::array<float, 4>, 4>;
 using Object = std::vector<std::vector<std::vector<sf::Vertex>>>;
@@ -33,6 +34,10 @@ struct Vector4f{
 
 float getDotProduct(sf::Vector3f v1, sf::Vector3f v2){
     return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+}
+
+sf::Vector3f getCrossProduct(sf::Vector3f v1, sf::Vector3f v2){
+    return sf::Vector3f(v1.y*v2.z - v2.y*v1.z,-(v1.x*v2.z - v1.z*v2.x),v1.x*v2.y - v2.x*v1.y);
 }
 
 sf::Vector3f operator*(const Matrix& mat, const sf::Vector3f& vec) {
@@ -198,10 +203,18 @@ sf::Vector3f canvasToViewPort(int x,int y){
     return sf::Vector3f(Vx,Vy,1);
 }
 
-sf::Vertex getPixel(int x,int y,sf::Color color){
+sf::Vertex getPixel(int x,int y,sf::Color color,DEPTH_BUFFER& depth_buffer,float z_inverse){
     float Sx = WIDTH/2 + x;
     float Sy = HEIGHT/2 - y;
-    return sf::Vertex (sf::Vector2f(Sx,Sy),color);
+
+    if(Sx < WIDTH && Sy < HEIGHT && Sx > 0 && Sy > 0){
+        if(z_inverse > depth_buffer[Sx][Sy]){
+            depth_buffer[Sx][Sy] = z_inverse;
+            return sf::Vertex (sf::Vector2f(Sx,Sy),color);
+        }
+    }
+
+    return sf::Vertex(sf::Vector2f(Sx,Sy),sf::Color::Transparent);
 }
 
 void translateVertexes(std::vector<sf::Vector3f>& vertexes, sf::Vector3f translation){
@@ -272,8 +285,15 @@ std::vector<float> interpolate(float d1, float d2, float i1, float i2){
     return values;
 }
 
-Range drawLine(sf::VertexArray& scene,sf::Vector2f p1,sf::Vector2f p2, sf::Color color,float h1, float h2){
+void swap(float& n1, float& n2){
+    float temp = n1;
+    n1 = n2;
+    n2 = temp;
+}
+
+Range drawLine(sf::VertexArray& scene,DEPTH_BUFFER& depth_buffer,sf::Vector2f p1,sf::Vector2f p2, sf::Color color,float h1, float h2,float z1, float z2){
     int i_start = 0;
+    sf::Vertex vertex;
     if(!(scene.getVertexCount() == 0)){
         i_start = scene.getVertexCount() - 1;
     }
@@ -282,12 +302,18 @@ Range drawLine(sf::VertexArray& scene,sf::Vector2f p1,sf::Vector2f p2, sf::Color
         if(p1.x > p2.x){
             swap(p1,p2);
             swap(h1,h2);
+            swap(z1,z2);
         }
         i_end = i_start + p2.x - p1.x;
         auto h_values = interpolate(h1,h2,p1.x,p2.x);
         auto values = interpolate(p1.y,p2.y,p1.x,p2.x);
+        auto z_values = interpolate(z1,z2,p1.x,p2.x);
         for(int x = p1.x; x <= p2.x; x++){
-            scene.append(getPixel(x,values[x-p1.x],multiplyColorWithIntensity(color,h_values[x-p1.x])));
+            float z = z_values[x - p1.x];
+            vertex = getPixel(x,values[x-p1.x],multiplyColorWithIntensity(color,h_values[x-p1.x]),depth_buffer,z);
+            if(!(vertex.color == sf::Color::Transparent)){
+                scene.append(vertex);
+            }
         }
     }
 
@@ -295,69 +321,89 @@ Range drawLine(sf::VertexArray& scene,sf::Vector2f p1,sf::Vector2f p2, sf::Color
         if(p1.y > p2.y){
             swap(p1,p2);
             swap(h1,h2);
+            swap(z1,z2);
         }
         i_end = i_start + p2.y - p1.y;
         auto h_values = interpolate(h1,h2,p1.y,p2.y);
         auto values = interpolate(p1.x,p2.x,p1.y,p2.y);
+        auto z_values = interpolate(z1,z2,p1.y,p2.y);
         for(int y = p1.y; y <= p2.y; y++){
-            scene.append(getPixel(values[y-p1.y],y,multiplyColorWithIntensity(color,h_values[y-p1.y])));
+            float z = z_values[y - p1.y];
+            vertex = getPixel(values[y-p1.y],y,multiplyColorWithIntensity(color,h_values[y-p1.y]),depth_buffer,z);
+            if(!(vertex.color == sf::Color::Transparent)){
+                scene.append(vertex);
+            }
         }
     }
+
     return {i_start,i_end};
 };
 
-// std::vector<std::vector<sf::Vertex>> getTriangle(sf::Vector2f l0, sf::Vector2f l1, sf::Vector2f l2, sf::Color color,float h0, float h1, float h2){
-//     if(l1.y < l0.y){
-//         swap(l0,l1);
-//         swap(h0,h1);
-//     }
-//     if(l2.y < l0.y){
-//         swap(l0,l2);
-//         swap(h0,h2);
-//     }
-//     if(l2.y < l1.y){
-//         swap(l1,l2);
-//         swap(h1,h2);
-//     }
-//     std::vector<std::vector<sf::Vertex>> lines;
-//     lines.reserve(abs(l2.y - l0.y) + 1);
+Range drawTriangle(sf::VertexArray& scene,DEPTH_BUFFER& depth_buffer,TempTriangle& triangle,float h0, float h1, float h2,float viewport_z){
+    sf::Vector2f l0 = projectVector(triangle.first[0],viewport_z);
+    float z0 = triangle.first[0].z;
+    sf::Vector2f l1 = projectVector(triangle.first[1],viewport_z);
+    float z1 = triangle.first[1].z;
+    sf::Vector2f l2 = projectVector(triangle.first[2],viewport_z);
+    float z2 = triangle.first[2].z;
+    sf::Color color = triangle.second;
 
-//     auto x_02 = interpolate(l0.x,l2.x,l0.y,l2.y);
-//     auto h_02 = interpolate(h0,h2,l0.y,l2.y);
-//     auto x_01 = interpolate(l0.x,l1.x,l0.y,l1.y);
-//     auto h_01 = interpolate(h0,h1,l0.y,l1.y);
-//     auto x_12 = interpolate(l1.x,l2.x,l1.y,l2.y);
-//     auto h_12 = interpolate(h1,h2,l1.y,l2.y);
+    int i_start = scene.getVertexCount() - 1;
+
+    if(l1.y < l0.y){
+        swap(l0,l1);
+        swap(h0,h1);
+        swap(z0,z1);
+    }
+    if(l2.y < l0.y){
+        swap(l0,l2);
+        swap(h0,h2);
+        swap(z0,z2);
+    }
+    if(l2.y < l1.y){
+        swap(l1,l2);
+        swap(h1,h2);
+        swap(z1,z2);
+    }
+    std::vector<std::vector<sf::Vertex>> lines;
+    lines.reserve(abs(l2.y - l0.y) + 1);
+
+    auto x_02 = interpolate(l0.x,l2.x,l0.y,l2.y);
+    auto h_02 = interpolate(h0,h2,l0.y,l2.y);
+    auto x_01 = interpolate(l0.x,l1.x,l0.y,l1.y);
+    auto h_01 = interpolate(h0,h1,l0.y,l1.y);
+    auto x_12 = interpolate(l1.x,l2.x,l1.y,l2.y);
+    auto h_12 = interpolate(h1,h2,l1.y,l2.y);
+    auto z_02 = interpolate(1/z0,1/z2,l0.y,l2.y);
+    auto z_01 = interpolate(1/z0,1/z1,l0.y,l1.y);
+    auto z_12 = interpolate(1/z1,1/z2,l1.y,l2.y);
     
-//     x_01.pop_back();
-//     h_01.pop_back();
-//     for(float h:h_12){
-//         h_01.push_back(h);
-//     }
-//     for(float l:x_12){
-//         x_01.push_back(l);
-//     }
-//     for(int y = l0.y; y <= l2.y; y++){
-//         lines.push_back(drawLine(sf::Vector2f(x_02[y -l0.y],y),sf::Vector2f(x_01[y - l0.y],y),color,h_02[y-l0.y],h_01[y-l0.y]));
-//     }
+    x_01.pop_back();
+    h_01.pop_back();
+    z_01.pop_back();
+    for(float h:h_12){
+        h_01.push_back(h);
+    }
+    for(float l:x_12){
+        x_01.push_back(l);
+    }
+    for (float z:z_12){
+        z_01.push_back(z);
+    }
 
-//     return lines;
-// }
+    int i_end = i_start + (l2.y - l0.y);
+    for(int y = l0.y; y <= l2.y; y++){
+        drawLine(scene,depth_buffer,sf::Vector2f(x_02[y -l0.y],y),sf::Vector2f(x_01[y - l0.y],y),color,h_02[y-l0.y],h_01[y-l0.y],z_02[y - l0.y],z_01[y-l0.y]);
+    }
 
-std::array<Range,3> drawTriangleWireFrame(sf::VertexArray& scene,Triangle triangle,std::vector<sf::Vector2f>& projected){
-    std::array<Range,3> ranges;
-    ranges[0] = drawLine(scene,projected[triangle.first[0]],projected[triangle.first[1]],triangle.second,1,1);
-    ranges[1] = drawLine(scene,projected[triangle.first[1]],projected[triangle.first[2]],triangle.second,1,1);
-    ranges[2] = drawLine(scene,projected[triangle.first[2]],projected[triangle.first[0]],triangle.second,1,1);
-
-    return ranges;
+    return {i_start,i_end};
 }
 
-std::array<Range,3> drawTriangleWireFrame(sf::VertexArray& scene,TempTriangleProjected triangle){
+std::array<Range,3> drawTriangleWireFrame(sf::VertexArray& scene,DEPTH_BUFFER& depth_buffer,TempTriangleProjected triangle){
     std::array<Range,3> ranges;
-    ranges[0] = drawLine(scene,triangle.first[0],triangle.first[1],triangle.second,1,1);
-    ranges[1] = drawLine(scene,triangle.first[1],triangle.first[2],triangle.second,1,1);
-    ranges[2] = drawLine(scene,triangle.first[2],triangle.first[0],triangle.second,1,1);
+    ranges[0] = drawLine(scene,depth_buffer,triangle.first[0],triangle.first[1],triangle.second,1,1,1,1);
+    ranges[1] = drawLine(scene,depth_buffer,triangle.first[1],triangle.first[2],triangle.second,1,1,1,1);
+    ranges[2] = drawLine(scene,depth_buffer,triangle.first[2],triangle.first[0],triangle.second,1,1,1,1);
     
     return ranges;
 }
@@ -456,8 +502,24 @@ int clipTriangle(TempTriangle& triangle, std::pair<sf::Vector3f,float> plane, Te
     return number_of_triangles;
 }
 
+bool isTriangleVisible(const TempTriangle& triangle) {
+    const auto& a = triangle.first[0];
+    const auto& b = triangle.first[1];
+    const auto& c = triangle.first[2];
+
+    sf::Vector3f normal = getCrossProduct(b - a, c - a);
+    sf::Vector3f centre = (a + b + c) / 3.f;
+
+    return getDotProduct(normal, -centre) > 0.f;
+}
+
 int main(){
     sf::RenderWindow window(sf::VideoMode({WIDTH,HEIGHT}), "Rasterizer");
+    DEPTH_BUFFER depth_buffer;
+    depth_buffer.resize(WIDTH);
+    for(auto& col: depth_buffer){
+        col.resize(HEIGHT,0);
+    }
     sf::Clock fps_clock;
     const sf::Font font("assets/PoetsenOne-Regular.ttf");
     sf::Text fps(font);
@@ -480,50 +542,55 @@ int main(){
     };
 
     Model cube {{
-                { 1,  1,  1},  // 0
-                {-1,  1,  1},  // 1
-                {-1, -1,  1},  // 2
-                { 1, -1,  1},  // 3
+        // Front face (z = +1), CCW from outside (+z looking toward -z)
+        { 1,  1,  1},  // 0  top-right-front
+        {-1,  1,  1},  // 1  top-left-front
+        {-1, -1,  1},  // 2  bottom-left-front
+        { 1, -1,  1},  // 3  bottom-right-front
 
-                { 1,  1, -1},  // 4
-                {-1,  1, -1},  // 5
-                {-1, -1, -1},  // 6
-                { 1, -1, -1}   // 7
-            },
+        // Back face (z = -1)
+        { 1,  1, -1},  // 4  top-right-back
+        {-1,  1, -1},  // 5  top-left-back
+        {-1, -1, -1},  // 6  bottom-left-back
+        { 1, -1, -1}   // 7  bottom-right-back
+    },
+    {
+        // +Z
+        {{0, 1, 2}, sf::Color::Red},
+        {{0, 2, 3}, sf::Color::Red},
 
-            {
-                {{0, 1, 2}, sf::Color::Red},
-                {{0, 2, 3}, sf::Color::Red},
+        // +X
+        {{4, 0, 3}, sf::Color::Green},
+        {{4, 3, 7}, sf::Color::Green},
 
-                {{4, 0, 3}, sf::Color::Green},
-                {{4, 3, 7}, sf::Color::Green},
+        // -Z
+        {{5, 7, 6}, sf::Color::Blue},
+        {{5, 4, 7}, sf::Color::Blue},
 
-                {{5, 4, 7}, sf::Color::Blue},
-                {{5, 7, 6}, sf::Color::Blue},
+        // -X
+        {{1, 6, 2}, sf::Color::Yellow},
+        {{1, 5, 6}, sf::Color::Yellow},
 
-                {{1, 5, 6}, sf::Color::Yellow},
-                {{1, 6, 2}, sf::Color::Yellow},
+        // +Y
+        {{1, 4, 5}, sf::Color::Magenta},
+        {{1, 0, 4}, sf::Color::Magenta},
 
-                {{4, 5, 1}, sf::Color::Magenta},
-                {{4, 1, 0}, sf::Color::Magenta},
-
-                {{2, 6, 7}, sf::Color::Cyan},
-                {{2, 7, 3}, sf::Color::Cyan}
-            }
-        };
+        // -Y
+        {{2, 7, 3}, sf::Color::Cyan},
+        {{2, 6, 7}, sf::Color::Cyan}
+    }};
 
     Model triangle{{
-                { 1.5f,  1.0f,  0.5f},  // 0
-                {-1.2f, -0.8f,  0.2f},  // 1
-                { 0.3f, -1.4f, -0.9f}   // 2
-            },
+        { 1.5f,  1.0f,  0.5f},  // 0
+        {-1.2f, -0.8f,  0.2f},  // 1
+        { 0.3f, -1.4f, -0.9f}   // 2
+    },
+    {
+        {{0, 1, 2}, sf::Color::Red}
+    }};
 
-            {
-                {{0, 1, 2}, sf::Color::Red}
-            }
-        };
-
-    std::vector<std::array<Range,3>> ranges;
+    std::vector<std::array<Range,3>> wireframe_ranges;
+    std::vector<Range> ranges;
     std::vector<Instance> instances;
 
     // instances.push_back(
@@ -605,6 +672,12 @@ instances.push_back(
     std::vector<TempTriangleProjected> projected_temp_triangles_buff;
 
     while (window.isOpen()) {
+        for(auto& col:depth_buffer){
+            for(auto& z: col){
+                z = 0;
+            }
+        }
+
         float dt = fps_clock.getElapsedTime().asSeconds();
         fps_clock.restart();
 
@@ -675,19 +748,27 @@ instances.push_back(
                 vertices_buff.push_back(final_transform*vertex);
             }
 
-            if(relative_position == ObjectRelativePositionToPlane::CompletelyInside){
-                for(auto& vertex:vertices_buff){
-                    projected_triangles_buff.push_back(projectVector(vertex,viewport_pos.z));
-                }
-                for(auto& triangle:instance.model_ptr->triangles){
-                    ranges.push_back(drawTriangleWireFrame(scene,triangle,projected_triangles_buff));
+            triangle_buff1.clear();
+            for(auto& triangle:instance.model_ptr->triangles){
+                TempTriangle temp_triangle = getTempTriangle(triangle,vertices_buff);
+                // sf::Vector3f camera_orientation_from_origin = sf::Vector3f(std::cos(camera_orientation[0])*std::sin(camera_orientation[1]),std::sin(camera_orientation[0]),std::cos(camera_orientation[1])*std::cos(camera_orientation[0]));
+                // sf::Vector3f camera_vector = camera_orientation_from_origin + camera_pos;
+                if(isTriangleVisible(temp_triangle)){
+                    triangle_buff1.push_back(temp_triangle);
                 }
             }
-            else if (relative_position == ObjectRelativePositionToPlane::InBetween){
-                triangle_buff1.clear();
-                for(auto& triangle:instance.model_ptr->triangles){
-                    triangle_buff1.push_back(getTempTriangle(triangle,vertices_buff));
+
+            if(relative_position == ObjectRelativePositionToPlane::CompletelyInside){
+                // for(auto& triangle: triangle_buff1){
+                //     projected_temp_triangles_buff.push_back(projectTriangle(triangle,viewport_pos.z));
+                // }
+
+                for (auto& triangle: triangle_buff1){
+                    ranges.push_back(drawTriangle(scene,depth_buffer,triangle,1,1,1,viewport_pos.z));
                 }
+
+            }
+            else if (relative_position == ObjectRelativePositionToPlane::InBetween){
 
                 for (auto& plane: clipping_volume){
                     for (auto& triangle: triangle_buff1){
@@ -700,13 +781,12 @@ instances.push_back(
                     swap(triangle_buff1,triangle_buff2);
                     triangle_buff2.clear();
                 }
-                projected_temp_triangles_buff.reserve(triangle_buff1.size());
-                for(auto& triangle: triangle_buff1){
-                    projected_temp_triangles_buff.push_back(projectTriangle(triangle,viewport_pos.z));
-                }
+                // for(auto& triangle: triangle_buff1){
+                //     projected_temp_triangles_buff.push_back(projectTriangle(triangle,viewport_pos.z));
+                // }
 
-                for (auto& triangle: projected_temp_triangles_buff){
-                    ranges.push_back(drawTriangleWireFrame(scene,triangle));
+                for (auto& triangle: triangle_buff1){
+                    ranges.push_back(drawTriangle(scene,depth_buffer,triangle,1,1,1,viewport_pos.z));
                 }
             }
 
@@ -718,7 +798,7 @@ instances.push_back(
         window.draw(scene);
         window.draw(fps);
 
-        ranges.clear();
+        wireframe_ranges.clear();
         scene.clear();
         
         window.display();
